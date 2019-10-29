25aaa5fc237a244025bd54b008b6901db27377cf
hchittanuru3 <hemanthc98@outlook.com>
2019-10-29 12:36:47 -0400

    Feature Representation V1 (Not tested)

--- server.py
+++ server.py
@@ -2,15 +2,18 @@ from flask import Flask, render_template, request, send_file
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def reload():
-	return render_template("index.html")
+    return render_template("index.html")
 
-@app.route('/get_match', methods=['POST'])
+
+@app.route("/get_match", methods=["POST"])
 def get_match():
-	pic = request.form['pic']
-	print("pic: " + str(pic))
-	return send_file(pic, mimetype='image')
+    pic = request.form["pic"]
+    print("pic: " + str(pic))
+    return send_file(pic, mimetype="image")
+
 
 if __name__ == "__main__":
-	app.run(debug=True, port=5000)
+    app.run(debug=True, port=5000)
