ad6d0da363dbd6f5c0daa8e879107729f6104a99
hchittanuru3 <hemanthc98@outlook.com>
2019-10-30 16:39:03 -0400

    Gitignore update

--- feature_representation.py
+++ feature_representation.py
@@ -8,6 +8,7 @@ def feature_extraction(img, mask):
     # Get color representation
     # Get texture representation
     # Fuse them
+    pass
 
 
 def get_color_features(img, mask):
@@ -18,26 +19,32 @@ def get_color_features(img, mask):
     # rs = cv2.cvtColor(im, cv2.COLOR_BGR2YCrCb)
     color_features = np.array(
         [
-            np.mean(red),
+            np.nanmean(red),
             np.std(red),
-            np.mean(green),
+            np.nanmean(green),
             np.std(green),
-            np.mean(blue),
-            np.std(blue)
+            np.nanmean(blue),
+            np.std(blue),
         ]
     )
     return color_features
 
 
 def get_hsv_features(img, mask):
-    pass
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    hist = cv2.calcHist([img], [0, 1, 2], None, (8, 8, 8), [0, 180, 0, 256, 0, 256])
+    cv2.normalize(hist, hist)
+    return hist.flatten()
 
 
 def get_glcm_features(img, mask):
     # https://stackoverflow.com/questions/47591359/calculating-the-co-occurrence-matrix-of-part-of-an-image-from-a-mask
     img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    glcm = feature.greycomatrix(img, [1], [0, np.pi/4, np.pi/2, 3*np.pi/4], symmetric=True, normed=True)
+    glcm = feature.greycomatrix(
+        img, [1], [0, np.pi / 4, np.pi / 2, 3 * np.pi / 4], symmetric=True, normed=True
+    )
     return greycoprops(glcm)
+
 
 def get_gabor_features(img, mask):
     pass
--- get_data.sh
+++ get_data.sh
@@ -6,6 +6,6 @@ wget "http://vision.stanford.edu/aditya86/ImageNetDogs/images.tar"
 
 unzip -d "cats/" "CAT_DATASET_01.zip"
 unzip -d "cats/" "CAT_DATASET_02.zip"
-tar -xvf "images.tar" -d "/dogs"
+tar -xvf "images.tar" -d "/dogs" 
 
 python transform_data.py
