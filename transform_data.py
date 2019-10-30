383a0cd329eb37b1e76fb3ced512d1e18c716135
Kenny Scharm <kscharm3@gatech.edu>
2019-10-30 18:42:06 -0400

    Finished data extraction and split

--- transform_data.py
+++ transform_data.py
@@ -1,7 +1,8 @@
 # /usr/bin/python
 import numpy as np
 import os
-import cv2
+from shutil import copy
+from random import shuffle
 
 
 def get_data(dataset_dir):
@@ -10,26 +11,32 @@ def get_data(dataset_dir):
         for file in os.listdir(d[0]):
             if file.endswith(".jpg"):
                 images_list.append(os.path.join(os.getcwd(), d[0], file))
+    shuffle(images_list)
     return images_list
 
 
 def split_data(dataset, split_val):
-    os.mkdir("train")
-    os.mkdir("test")
+    if not os.path.exists("train"):
+        os.mkdir("train")
+    if not os.path.exists("test"):
+        os.mkdir("test")
     i = 0
     while i < len(dataset):
-        img = cv2.imread(dataset[i])
         if i < int(split_val * len(dataset)):
-            cv2.imwrite(os.path.join("train", dataset[i]), img)
+            copy(dataset[i], "./train")
         else:
-            cv2.imwrite(os.path.join("test", dataset[i]), img)
+            copy(dataset[i], "./test")
         i = i + 1
+    os.chdir("..")
 
 
 if __name__ == "__main__":
     dog_path = "./dogs/images/Images"
-    # cat_path = "./cats/"
+    cat_path = "./cats/"
     dog_data = get_data(dog_path)
-    # cat_data = get_data(cat_path)
+    cat_data = get_data(cat_path)
     os.chdir("./dogs")
-    split_data(dog_data, 0.9)
+    split_data(dog_data, 0.95)
+    os.chdir("./cats")
+    split_data(cat_data, 0.95)
+
