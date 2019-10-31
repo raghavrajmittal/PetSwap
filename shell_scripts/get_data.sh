7b33aadf0b47aa67e393f029194baf62aabef47c
hchittanuru3 <hemanthc98@outlook.com>
2019-10-31 12:25:28 -0400

    Update

--- shell_scripts/get_data.sh
+++ shell_scripts/get_data.sh
@@ -1,4 +1,4 @@
-#/usr/bin/bash
+#!/usr/bin/bash
 
 wget "https://archive.org/download/CAT_DATASET/CAT_DATASET_01.zip"
 wget "https://archive.org/download/CAT_DATASET/CAT_DATASET_02.zip"
@@ -6,6 +6,6 @@ wget "http://vision.stanford.edu/aditya86/ImageNetDogs/images.tar"
 
 unzip "CAT_DATASET_01.zip" -d "cats/" 
 unzip "CAT_DATASET_02.zip" -d "cats/" 
-tar -xvf "images.tar" -C "dogs" 
+tar -xvf "images.tar" -C "/dogs" 
 
 python transform_data.py
