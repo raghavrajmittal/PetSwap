39294ffc3b9c416b07ea3f5f594d5374b1d5d9f5
hchittanuru3 <hemanthc98@outlook.com>
2019-10-30 23:45:13 -0400

    Feature representation for midterm update

--- feature_representation.py
+++ feature_representation.py
@@ -1,30 +1,45 @@
 import cv2
 import numpy as np
 from skimage.feature import greycomatrix, greycoprops
+from skimage import io
+import maskrcnn
 
 
-def feature_extraction(img, mask):
+def feature_extraction(img):
     # Get pixels that are masked
+    results = maskrcnn.get_masks(img)
+    mask = results[0]["masks"]
     # Get color representation
+    color_features = get_color_features(img, mask)
+    hist_features = get_hsv_features(img, mask)
     # Get texture representation
+    glcm_features = get_glcm_features(img, mask)
     # Fuse them
-    pass
+    return np.concatenate((color_features, hist_features, glcm_features[0]), axis=None)
 
 
 def get_color_features(img, mask):
+    other = cv2.cvtColor(img, cv2.COLOR_BGR2YCrCb)
     img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    img = img.astype(float)
+    other = other.astype(float)
+    mask = np.tile(mask, (1, 1, 3))
+    img[~mask] = np.NaN
+    other[~mask] = np.NaN
     red = img[:, :, 0]
     green = img[:, :, 1]
     blue = img[:, :, 2]
-    # rs = cv2.cvtColor(im, cv2.COLOR_BGR2YCrCb)
+
     color_features = np.array(
         [
             np.nanmean(red),
-            np.std(red),
+            np.nanstd(red),
             np.nanmean(green),
-            np.std(green),
+            np.nanstd(green),
             np.nanmean(blue),
-            np.std(blue),
+            np.nanstd(blue),
+            np.nanmean(other),
+            np.nanstd(other),
         ]
     )
     return color_features
@@ -32,19 +47,32 @@ def get_color_features(img, mask):
 
 def get_hsv_features(img, mask):
     img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-    hist = cv2.calcHist([img], [0, 1, 2], None, (8, 8, 8), [0, 180, 0, 256, 0, 256])
-    cv2.normalize(hist, hist)
-    return hist.flatten()
+    mask = np.tile(mask, (1, 1, 3))
+    hist_mask = np.zeros(mask.shape).astype(np.uint8)
+    hist_mask[~mask] = 0
+    hist_mask[mask] = 255
+    h = cv2.calcHist([img], channels=[0], mask=hist_mask[:,:,0], histSize=[8], ranges=[0, 8])/np.max(img[:,:,0])
+    s = cv2.calcHist([img], channels=[1], mask=hist_mask[:,:,1], histSize=[2], ranges=[0, 2])/np.max(img[:,:,1])
+    v = cv2.calcHist([img], channels=[2], mask=hist_mask[:,:,2], histSize=[2], ranges=[0, 2])/np.max(img[:,:,2])
+    return np.concatenate((h, s, v), axis = None)
 
 
 def get_glcm_features(img, mask):
-    # https://stackoverflow.com/questions/47591359/calculating-the-co-occurrence-matrix-of-part-of-an-image-from-a-mask
     img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    glcm = feature.greycomatrix(
-        img, [1], [0, np.pi / 4, np.pi / 2, 3 * np.pi / 4], symmetric=True, normed=True
-    )
+    mask = np.squeeze(mask)
+    img[~mask] = -1
+    glcm = greycomatrix(
+        img,
+        [1],
+        [0, np.pi / 4, np.pi / 2, 3 * np.pi / 4],
+        symmetric=True,
+        normed=True,
+    )[1:, 1:, :, :]
     return greycoprops(glcm)
 
 
 def get_gabor_features(img, mask):
     pass
+
+
+print(feature_extraction(io.imread("test_cat.jpg")))
--- maskrcnn.py
+++ maskrcnn.py
@@ -9,88 +9,165 @@ import matplotlib
 import matplotlib.pyplot as plt
 import argparse
 
-ROOT_DIR = os.path.abspath("mask/") # Root directory of the project
-sys.path.append(ROOT_DIR)  # To find local version of the library
-sys.path.append(os.path.join(ROOT_DIR, "samples/coco/"))  # To find local version
-MODEL_DIR = os.path.join(ROOT_DIR, "logs") # Directory to save logs and trained model
-COCO_MODEL_PATH = os.path.join(ROOT_DIR, "mask_rcnn_coco.h5") # Local path to trained weights file
-from mrcnn import utils
-import mrcnn.model as modellib
-from mrcnn import visualize
-import coco
-if not os.path.exists(COCO_MODEL_PATH): # Download COCO trained weights from Releases if needed
-    utils.download_trained_weights(COCO_MODEL_PATH)
-IMAGE_DIR = os.path.join(ROOT_DIR, "images")
 
+def get_masks(img):
 
-# in case video needs to be saved
-ap = argparse.ArgumentParser()
-ap.add_argument("-i", "--images", default=os.path.join(ROOT_DIR, "images"),
-	help="path of the image directory")
-args = vars(ap.parse_args())
-IMAGE_DIR = args['images']
+    ROOT_DIR = os.path.abspath("mask/")  # Root directory of the project
+    sys.path.append(ROOT_DIR)  # To find local version of the library
+    sys.path.append(os.path.join(ROOT_DIR, "samples/coco/"))  # To find local version
+    MODEL_DIR = os.path.join(
+        ROOT_DIR, "logs"
+    )  # Directory to save logs and trained model
+    COCO_MODEL_PATH = os.path.join(
+        ROOT_DIR, "mask_rcnn_coco.h5"
+    )  # Local path to trained weights file
+    from mrcnn import utils
+    import mrcnn.model as modellib
+    from mrcnn import visualize
+    import coco
 
+    if not os.path.exists(
+        COCO_MODEL_PATH
+    ):  # Download COCO trained weights from Releases if needed
+        utils.download_trained_weights(COCO_MODEL_PATH)
+    IMAGE_DIR = os.path.join(ROOT_DIR, "images")
 
+    # in case video needs to be saved
+    ap = argparse.ArgumentParser()
+    ap.add_argument(
+        "-i",
+        "--images",
+        default=os.path.join(ROOT_DIR, "images"),
+        help="path of the image directory",
+    )
+    args = vars(ap.parse_args())
+    IMAGE_DIR = args["images"]
 
-# ## Configurations
-# We'll be using a model trained on the MS-COCO dataset. The configurations of this model are in the ```CocoConfig``` class in ```coco.py```.
-# For inferencing, modify the configurations a bit to fit the task. To do so, sub-class the ```CocoConfig``` class and override the attributes you need to change.
-class InferenceConfig(coco.CocoConfig):
-    # Batch size = GPU_COUNT * IMAGES_PER_GPU
-    GPU_COUNT = 1
-    IMAGES_PER_GPU = 1
-config = InferenceConfig()
-config.display()
+    # ## Configurations
+    # We'll be using a model trained on the MS-COCO dataset. The configurations of this model are in the ```CocoConfig``` class in ```coco.py```.
+    # For inferencing, modify the configurations a bit to fit the task. To do so, sub-class the ```CocoConfig``` class and override the attributes you need to change.
+    class InferenceConfig(coco.CocoConfig):
+        # Batch size = GPU_COUNT * IMAGES_PER_GPU
+        GPU_COUNT = 1
+        IMAGES_PER_GPU = 1
 
+    config = InferenceConfig()
+    config.display()
 
+    # ## Create Model and Load Trained Weights
+    model = modellib.MaskRCNN(
+        mode="inference", model_dir=MODEL_DIR, config=config
+    )  # Create model object in inference mode.
+    model.load_weights(COCO_MODEL_PATH, by_name=True)  # Load weights trained on MS-COCO
 
-# ## Create Model and Load Trained Weights
-model = modellib.MaskRCNN(mode="inference", model_dir=MODEL_DIR, config=config) # Create model object in inference mode.
-model.load_weights(COCO_MODEL_PATH, by_name=True) # Load weights trained on MS-COCO
+    # ## Class Names
+    # To improve consistency, and to support training on data from multiple sources at the same time, our ```Dataset``` class assigns it's own sequential integer IDs to each class. For example, if you load the COCO dataset using our ```Dataset``` class, the 'person' class would get class ID = 1 (just like COCO) and the 'teddy bear' class is 78 (different from COCO). Keep that in mind when mapping class IDs to class names.
+    # To get the list of class names, you'd load the dataset and then use the ```class_names``` property like this.
+    # dataset = coco.CocoDataset()
+    # dataset.load_coco(COCO_DIR, "train")
+    # dataset.prepare()
+    # print(dataset.class_names)
+    # COCO Class names - Instead of downloading, we're including the list of class names below
+    # Index of the class in the list is its ID.
+    class_names = [
+        "BG",
+        "person",
+        "bicycle",
+        "car",
+        "motorcycle",
+        "airplane",
+        "bus",
+        "train",
+        "truck",
+        "boat",
+        "traffic light",
+        "fire hydrant",
+        "stop sign",
+        "parking meter",
+        "bench",
+        "bird",
+        "cat",
+        "dog",
+        "horse",
+        "sheep",
+        "cow",
+        "elephant",
+        "bear",
+        "zebra",
+        "giraffe",
+        "backpack",
+        "umbrella",
+        "handbag",
+        "tie",
+        "suitcase",
+        "frisbee",
+        "skis",
+        "snowboard",
+        "sports ball",
+        "kite",
+        "baseball bat",
+        "baseball glove",
+        "skateboard",
+        "surfboard",
+        "tennis racket",
+        "bottle",
+        "wine glass",
+        "cup",
+        "fork",
+        "knife",
+        "spoon",
+        "bowl",
+        "banana",
+        "apple",
+        "sandwich",
+        "orange",
+        "broccoli",
+        "carrot",
+        "hot dog",
+        "pizza",
+        "donut",
+        "cake",
+        "chair",
+        "couch",
+        "potted plant",
+        "bed",
+        "dining table",
+        "toilet",
+        "tv",
+        "laptop",
+        "mouse",
+        "remote",
+        "keyboard",
+        "cell phone",
+        "microwave",
+        "oven",
+        "toaster",
+        "sink",
+        "refrigerator",
+        "book",
+        "clock",
+        "vase",
+        "scissors",
+        "teddy bear",
+        "hair drier",
+        "toothbrush",
+    ]
+    # print(class_names.index('cat')) # -> 16
+    # print(class_names.index('dog')) # -> 17
 
+    # ## Run Object Detection
+    # Load a random image from the images folder and visualize results
+    file_names = next(os.walk(IMAGE_DIR))[2]
+    # image = skimage.io.imread(os.path.join(IMAGE_DIR, random.choice(file_names)))
+    results = model.detect([img], verbose=1)  # Run detection
 
-
-# ## Class Names
-# To improve consistency, and to support training on data from multiple sources at the same time, our ```Dataset``` class assigns it's own sequential integer IDs to each class. For example, if you load the COCO dataset using our ```Dataset``` class, the 'person' class would get class ID = 1 (just like COCO) and the 'teddy bear' class is 78 (different from COCO). Keep that in mind when mapping class IDs to class names.
-# To get the list of class names, you'd load the dataset and then use the ```class_names``` property like this.
-# dataset = coco.CocoDataset()
-# dataset.load_coco(COCO_DIR, "train")
-# dataset.prepare()
-# print(dataset.class_names)
-# COCO Class names - Instead of downloading, we're including the list of class names below
-# Index of the class in the list is its ID.
-class_names = ['BG', 'person', 'bicycle', 'car', 'motorcycle', 'airplane',
-               'bus', 'train', 'truck', 'boat', 'traffic light',
-               'fire hydrant', 'stop sign', 'parking meter', 'bench', 'bird',
-               'cat', 'dog', 'horse', 'sheep', 'cow', 'elephant', 'bear',
-               'zebra', 'giraffe', 'backpack', 'umbrella', 'handbag', 'tie',
-               'suitcase', 'frisbee', 'skis', 'snowboard', 'sports ball',
-               'kite', 'baseball bat', 'baseball glove', 'skateboard',
-               'surfboard', 'tennis racket', 'bottle', 'wine glass', 'cup',
-               'fork', 'knife', 'spoon', 'bowl', 'banana', 'apple',
-               'sandwich', 'orange', 'broccoli', 'carrot', 'hot dog', 'pizza',
-               'donut', 'cake', 'chair', 'couch', 'potted plant', 'bed',
-               'dining table', 'toilet', 'tv', 'laptop', 'mouse', 'remote',
-               'keyboard', 'cell phone', 'microwave', 'oven', 'toaster',
-               'sink', 'refrigerator', 'book', 'clock', 'vase', 'scissors',
-               'teddy bear', 'hair drier', 'toothbrush']
-#print(class_names.index('cat')) # -> 16
-#print(class_names.index('dog')) # -> 17
-
-
-
-# ## Run Object Detection
-# Load a random image from the images folder and visualize results
-file_names = next(os.walk(IMAGE_DIR))[2]
-image = skimage.io.imread(os.path.join(IMAGE_DIR, random.choice(file_names)))
-results = model.detect([image], verbose=1) # Run detection
-
-# filter out non chosen classes
-chosen_classes = [16, 17] # class indices for cat, dog
-for r in results:
-    index_mask = np.isin(r['class_ids'], chosen_classes)
-    r['rois'] = r['rois'][index_mask]
-    r['class_ids'] = r['class_ids'][index_mask]
-    r['scores'] = r['scores'][index_mask]
-    r['masks'] = r['masks'][:, :, index_mask]
-# visualize.display_instances(image, r['rois'], r['masks'], r['class_ids'], class_names, r['scores'])
+    # filter out non chosen classes
+    chosen_classes = [16, 17]  # class indices for cat, dog
+    for r in results:
+        index_mask = np.isin(r["class_ids"], chosen_classes)
+        r["rois"] = r["rois"][index_mask]
+        r["class_ids"] = r["class_ids"][index_mask]
+        r["scores"] = r["scores"][index_mask]
+        r["masks"] = r["masks"][:, :, index_mask]
+    return results
+    # visualize.display_instances(image, r['rois'], r['masks'], r['class_ids'], class_names, r['scores'])
