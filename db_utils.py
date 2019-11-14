ac8e6cdbabf7afe02f8684ef14074a864580aac8
hchittanuru3 <hemanthc98@outlook.com>
2019-11-14 14:43:20 -0500

    DB code v1

--- db_utils.py
+++ db_utils.py
@@ -15,3 +15,17 @@ def get_client():
 
 def insert_features(features):
     client = get_client()
+    db = client.database
+    for feature in features:
+        insert_one_feature(db, feature)
+
+def insert_one_feature(db, feature_rep):
+    feature = {'file': feature_rep[0], 'type': feature_rep[1], 'features': feature_rep[2], 'cluster_id': feature_rep[3]}
+    result = db.features.insert_one(feature)
+    print("Entry created as " + str(result.inserted_id))
+
+'''
+client = get_client()
+db = client.database
+insert_one_feature(db, ("test", "test", "test"))
+'''
