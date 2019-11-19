71825efdb59611e7880f8d3b4fde0fc86bf5195b
hchittanuru3 <hemanthc98@outlook.com>
2019-11-19 16:29:43 -0500

    Fixed feature rep

--- feature_representation.py
+++ feature_representation.py
@@ -9,7 +9,7 @@ import maskrcnn
 def feature_extraction(img):
     # Get pixels that are masked
     results = maskrcnn.get_masks(img)
-    mask = results[0]["masks"]
+    mask = results[0]["masks"][:,:,0]
     # Get color representation
     color_features = get_color_features(img, mask)
     hist_features = get_hsv_features(img, mask)
@@ -24,8 +24,6 @@ def get_color_features(img, mask):
     img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     img = img.astype(float)
     other = other.astype(float)
-    if mask.shape[2] != 3:
-        mask = np.tile(mask, (1, 1, 3))
     img[~mask] = np.NaN
     other[~mask] = np.NaN
     red = img[:, :, 0]
@@ -49,25 +47,25 @@ def get_color_features(img, mask):
 
 def get_hsv_features(img, mask):
     img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-    mask = np.tile(mask, (1, 1, 3))
+    #mask = np.tile(mask, (1, 1, 3))
     hist_mask = np.zeros(mask.shape).astype(np.uint8)
     hist_mask[~mask] = 0
     hist_mask[mask] = 255
     h = cv2.calcHist(
-        [img], channels=[0], mask=hist_mask[:, :, 0], histSize=[8], ranges=[0, 8]
+        [img], channels=[0], mask=hist_mask, histSize=[8], ranges=[0, 8]
     ) / np.max(img[:, :, 0])
     s = cv2.calcHist(
-        [img], channels=[1], mask=hist_mask[:, :, 1], histSize=[2], ranges=[0, 2]
+        [img], channels=[1], mask=hist_mask, histSize=[2], ranges=[0, 2]
     ) / np.max(img[:, :, 1])
     v = cv2.calcHist(
-        [img], channels=[2], mask=hist_mask[:, :, 2], histSize=[2], ranges=[0, 2]
+        [img], channels=[2], mask=hist_mask, histSize=[2], ranges=[0, 2]
     ) / np.max(img[:, :, 2])
     return np.concatenate((h, s, v), axis=None)
 
 
 def get_glcm_features(img, mask):
     img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    mask = np.squeeze(mask)
+    #mask = np.squeeze(mask)
     img[~mask] = -1
     glcm = greycomatrix(
         img, [1], [0, np.pi / 4, np.pi / 2, 3 * np.pi / 4], symmetric=True, normed=True
@@ -79,6 +77,6 @@ def get_gabor_features(img, mask):
     pass
 
 if __name__ == '__main__':
-    img = io.imread('test_cat.jpg')
+    img = io.imread('dogs/train/n02106166_1429.jpg')
     feature_extraction(img)
 # print(feature_extraction(io.imread("test_cat.jpg")))
