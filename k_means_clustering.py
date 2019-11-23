f0facd782ad58941685fe4bd7489bc88d083ca7c
hchittanuru3 <hemanthc98@outlook.com>
2019-11-23 16:44:19 -0500

    Added kmeans SSE

--- k_means_clustering.py
+++ k_means_clustering.py
@@ -13,16 +13,38 @@ def cluster(features):
     centroids = kmeans.cluster_centers_
     return labels, centroids
 
-dog_dict = np.load('dog_features.npz')
-cat_dict = np.load('cat_features.npz')
-dog_files, dog_features = dog_dict['image_names'], dog_dict['dog_features']
-cat_files, cat_features = cat_dict['image_names'], cat_dict['cat_features']
-files = np.concatenate((dog_files, cat_files))
-print(dog_features.shape, cat_features.shape)
-features = np.concatenate((dog_features, cat_features), axis=0)
-labels, centroids = cluster(features)
-print(labels)
-print(files)
+def hypertune(features):
+    clusters = range(7, 25)
+    losses = []
+    for k in clusters:
+        kmeans = MiniBatchKMeans(n_clusters=k)
+        kmeans.fit(features)
+        labels = kmeans.labels_
+        centers = kmeans.cluster_centers_
+        dist = pairwise_distance(centers, features)
+        K, D = centers.shape
+        N, D = points.shape
+        dist = dist[cluster_idx, np.arange(N)]
+        losses.append(np.sum(dist))
+    plt.plot(clusters, losses)
+    plt.show()
+
+def pairwise_distance(x, y):
+    diffs = np.power(x[:, np.newaxis] - y[np.newaxis], 2)
+    sum_mat = np.sum(diffs, axis=2)
+    dist = np.sqrt(sum_mat)
+    return dist
+
+
+if __name__ == '__main__':
+    dog_dict = np.load('dog_features.npz')
+    cat_dict = np.load('cat_features.npz')
+    dog_files, dog_features = dog_dict['image_names'], dog_dict['dog_features']
+    cat_files, cat_features = cat_dict['image_names'], cat_dict['cat_features']
+    files = np.concatenate((dog_files, cat_files))
+    print(dog_features.shape, cat_features.shape)
+    features = np.concatenate((dog_features, cat_features), axis=0)
+    hyptertune(features)
 
 '''
 # replace with actual data
